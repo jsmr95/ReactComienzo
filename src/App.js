85152c91609073f76b18da5850919ea924cd773f
José María Gallego Martel <jmgallego@atsistemas.com>
2019-07-01 09:03:27 +0200

    Uso de estados e hijos en un componente de React

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render(){
+    return(
+      <div className="App">
+        <h1>Hola</h1>
+        <Person name="Jose" age="12" />
+        <Person name="Pepe" age="10" >My hobbies: jugar! </Person>
+      </div>
+    )
+      /*var h1 = React.createElement('h1', null, 'Hola soy React!!')
+      return React.createElement('div', {className: 'App'}, h1);*/
+
+  }
 }
 
 export default App;
